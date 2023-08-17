ba8bfcd0b324c9f5f7ae0cc3d4bd3b7f46c778db
wenchao <wenchao@ndsec.work>
2023-08-17 18:51:32 +0800

    [wc/fix] sonar verify excludes test process; start jvm with utf-8

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,6 @@ APP_LOG_NAME=app.log
 APP_JAR_NAME=server.jar
 APP_WD=/app
 
-nohup java -jar ${APP_WD}/${APP_JAR_NAME} > ${APP_LOG_NAME} 2>&1 &
+nohup java -jar -D"file.encoding=UTF-8" ${APP_WD}/${APP_JAR_NAME} > ${APP_LOG_NAME} 2>&1 &
 
 while true; do sleep 3600; done
