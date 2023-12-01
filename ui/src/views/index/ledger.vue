758d9126ba65a8c44dd0380346f692f0f06ffa9f
wenchao <aronstrider@gmail.com>
2023-12-02 02:44:55 +0800

    [wc/ui] re-layout ledger management page, mainly the position of the bottom bar

--- ui/src/views/index/ledger.vue
+++ ui/src/views/index/ledger.vue
@@ -1,71 +1,62 @@
 <template>
-    <modal-presentation @close-300="onclose">
-        <van-action-sheet v-model="show" title="Title">
-            <div class="page">
-                <div class="record-header">Edit Fields</div>
-                <van-cell-group class="shadow overflow-hidden br8 ">
-                    <van-field v-model="editLedgerName" type="text" label="name"/>
-                </van-cell-group>
-                <gap-component></gap-component>
-                <common-button @click="submitEditLedger"
-                               :disabled="editLedgerLoading">
-                    <template #default>Submit</template>
-                </common-button>
-            </div>
-        </van-action-sheet>
-        <van-action-sheet v-model="addLedgerShow" title="Title">
-            <div class="page">
-                <gap-component :value="'32px'"></gap-component>
-                <div class="record-header">Add Fields</div>
-                <van-cell-group class="shadow overflow-hidden br8 ">
-                    <van-field v-model="addLedgerName" type="text" label="name"/>
-                </van-cell-group>
-                <gap-component></gap-component>
-                <common-button @click="submitAddLedger"
-                               :disabled="addLedgerLoading">
-                    <template #default>Submit</template>
-                </common-button>
-            </div>
-        </van-action-sheet>
-        <div ref="page-main-frame" style="position: relative; height: 100%;">
-            <div class="page" style="overflow-y: scroll;" ref="page-main-area">
-                <div class="modal-title">Ledger Management</div>
-                <gap-component :value="'55px'"></gap-component>
-                <div class="record-header">Ledgers</div>
-                <van-cell-group class="shadow overflow-hidden br8">
-                    <van-swipe-cell v-for="ledger in ledgers">
-                        <van-cell :border="false" :title="ledger.name"/>
-                        <template #right>
-                            <van-button square type="primary" text="Edit"
-                                        @click="onClickEdit(ledger.id, ledger.name)"/>
-                            <van-button square type="danger" text="Delete"
-                                        @click="onClickDelete(ledger.id, ledger.name)"/>
-                        </template>
-                    </van-swipe-cell>
-                </van-cell-group>
-
-            </div>
+  <modal-presentation @close-300="onclose">
+    <van-action-sheet v-model="show">
+      <div class="page">
+        <div class="record-header">Edit Fields</div>
+        <van-cell-group class="shadow overflow-hidden br8 ">
+          <van-field v-model="editLedgerName" type="text" label="name"/>
+        </van-cell-group>
+        <gap-component></gap-component>
+        <common-button @click="submitEditLedger"
+                       :disabled="editLedgerLoading">
+          <template #default>Submit</template>
+        </common-button>
+      </div>
+    </van-action-sheet>
+    <van-action-sheet v-model="addLedgerShow" title="Title">
+      <div class="page">
+        <gap-component :value="'32px'"></gap-component>
+        <div class="record-header">Add Fields</div>
+        <van-cell-group class="shadow overflow-hidden br8 ">
+          <van-field v-model="addLedgerName" type="text" label="name"/>
+        </van-cell-group>
+        <gap-component></gap-component>
+        <common-button @click="submitAddLedger"
+                       :disabled="addLedgerLoading">
+          <template #default>Submit</template>
+        </common-button>
+      </div>
+    </van-action-sheet>
 
 
-            <div
-                ref="bottom-tool-bar"
-                style="position: absolute; bottom: 0;left: 0; right: 0; background-color: white; border-top: 1px solid #ebedf0; width: 100%">
+    <div ref="page-main-frame" style="overflow: scroll; height: 100%;">
+      <div class="page" ref="page-main-area" style="padding-bottom: 50px; overflow: scroll;" >
+        <div class="modal-title">Ledger Management</div>
+        <gap-component :value="'55px'"></gap-component>
+        <div class="record-header">Ledgers</div>
+        <van-cell-group class="shadow overflow-hidden br8">
+          <van-swipe-cell v-for="ledger in ledgers">
+            <van-cell :border="false" :title="ledger.name"/>
+            <template #right>
+              <van-button square type="primary" text="Edit"
+                          @click="onClickEdit(ledger.id, ledger.name)"/>
+              <van-button square type="danger" text="Delete"
+                          @click="onClickDelete(ledger.id, ledger.name)"/>
+            </template>
+          </van-swipe-cell>
+        </van-cell-group>
+      </div>
 
+      <div ref="bottom-tool-bar" class="bottom-bar" style="padding-left: 8px;">
+        <solid-icon
+            :clickable="true"
+            icon-class="ali-international-icon-add-1"
+            @click="ff">
+        </solid-icon>
+      </div>
+    </div>
 
-                <van-button style="
-                        background-color: #1989fa;
-                        color: white; border: 1px solid #1989fa; border-radius: 5px;"
-                            plain
-                            @click="submitEditLedger"
-                            :disabled="editLedgerLoading"
-                            ref="add-ledger-btn"
-                >
-                    <van-icon name="plus"></van-icon>
-                </van-button>
-            </div>
-        </div>
-
-    </modal-presentation>
+  </modal-presentation>
 </template>
 
 <script lang='ts'>
@@ -79,198 +70,262 @@ import Client from "@/request/client";
 import CommonButton from "@/views/components/CommonButton.vue";
 import GapComponent from "@/views/components/GapComponent.vue";
 import {unmountComponent} from "@/ts/utils";
+import SolidIcon from "@/views/components/SolidIcon.vue";
 
 class FormItem {
-    categoryValue: string | null = null;
-    amount: number | null = null;
-    count: number | null = null;
-    description: string | null = null;
+  categoryValue: string | null = null;
+  amount: number | null = null;
+  count: number | null = null;
+  description: string | null = null;
 }
 
 @Component({
-    components: {
-        GapComponent,
-        CommonButton,
-        ModalPresentation: ModalPresentationView
-    }
+  components: {
+    SolidIcon,
+    GapComponent,
+    CommonButton,
+    ModalPresentation: ModalPresentationView
+  }
 })
 export default class ManageLedgerView extends Vue {
-    beforeDestroy() {
-        console.log("destroyed")
+  beforeDestroy() {
+    console.log("destroyed")
+  }
+
+  onclose() {
+    unmountComponent(this, 500)
+  }
+
+  ledgers: any = []
+  varTable: any = {}
+  show = false
+
+  categoryValue: string | null = null
+  name = ""
+  editLedgerId: number | null = null
+  editLedgerName: string | null = ""
+
+
+  addLedgerLoading = false
+  addLedgerShow = false
+  addLedgerName: string | null = ""
+  countxxx = 100;
+
+  submitAddLedger() {
+    let name = this.addLedgerName
+    if (name == null || name === "") {
+      Notify({
+        type: "danger",
+        message: "name is empty"
+      })
+      return
     }
+    this.addLedgerLoading = true
+    Client.addLedger(name).then((resp) => {
+      Notify({
+        type: "success",
+        message: "Add success"
+      })
 
-    onclose() {
-        unmountComponent(this, 500)
-    }
+      this.addLedgerShow = false
+      this.ledgers.push({
+        id: resp.data.id,
+        name: resp.data.name,
+        ctime: resp.data.ctime
+      })
+      // we don't recover the loading status to prevent the user click submit button twice
+    }).catch(() => {
+      this.addLedgerLoading = false
+      Notify({
+        type: "danger",
+        message: "add failed"
+      })
+    })
 
-    ledgers: any = []
-    varTable: any = {}
-    show = false
+  }
 
-    categoryValue: string | null = null
-    name = ""
-    editLedgerId: number | null = null
-    editLedgerName: string | null = ""
+  onClickEdit(ledgerId: number, ledgerName: string) {
+    this.editLedgerId = ledgerId
+    this.editLedgerName = ledgerName
+    this.show = true
+  }
 
+  onClickDelete(ledgerId: number, ledgeName: string) {
+    Client.deleteLedger(ledgerId).then(() => {
+      Notify({
+        type: "success",
+        message: "Delete success"
+      })
+      this.ledgers = this.ledgers.filter((item: any) => item.id != ledgerId)
+      eventBus.$emit("ledger-deleted", ledgerId)
+    }).catch(() => {
+      Notify({
+        type: "danger",
+        message: "Delete failed"
+      })
+    })
+  }
 
-    addLedgerLoading = false
-    addLedgerShow = false
-    addLedgerName: string | null = ""
-    countxxx = 100;
+  editLedgerLoading = false
 
-    submitAddLedger() {
-        let name = this.addLedgerName
-        if (name == null || name === "") {
-            Notify({
-                type: "danger",
-                message: "name is empty"
-            })
-            return
+  submitEditLedger() {
+    this.editLedgerLoading = true
+    setTimeout(() => {
+      this.editLedgerLoading = false
+      Notify({
+        type: "success",
+        message: "Update success"
+      })
+
+      this.show = false
+      this.ledgers.filter((item: any) => {
+        if (item.id === this.editLedgerId) {
+          item.name = this.editLedgerName
         }
-        this.addLedgerLoading = true
-        Client.addLedger(name).then((resp) => {
-            Notify({
-                type: "success",
-                message: "Add success"
-            })
+      })
+    }, 500)
+  }
 
-            this.addLedgerShow = false
-            this.ledgers.push({
-                id: resp.data.id,
-                name: resp.data.name,
-                ctime: resp.data.ctime
-            })
-            // we don't recover the loading status to prevent the user click submit button twice
-        }).catch(() => {
-            this.addLedgerLoading = false
-            Notify({
-                type: "danger",
-                message: "add failed"
-            })
-        })
+  ledgersLoading = false
 
-    }
-
-    onClickEdit(ledgerId: number, ledgerName: string) {
-        this.editLedgerId = ledgerId
-        this.editLedgerName = ledgerName
-        this.show = true
-    }
-
-    onClickDelete(ledgerId: number, ledgeName: string) {
-        Client.deleteLedger(ledgerId).then(() => {
-            Notify({
-                type: "success",
-                message: "Delete success"
-            })
-            this.ledgers = this.ledgers.filter((item: any) => item.id != ledgerId)
-            eventBus.$emit("ledger-deleted", ledgerId)
-        }).catch(() => {
-            Notify({
-                type: "danger",
-                message: "Delete failed"
-            })
-        })
-    }
-
-    editLedgerLoading = false
-
-    submitEditLedger() {
-        this.editLedgerLoading = true
-        setTimeout(() => {
-            this.editLedgerLoading = false
-            Notify({
-                type: "success",
-                message: "Update success"
-            })
-
-            this.show = false
-            this.ledgers.filter((item: any) => {
-                if (item.id === this.editLedgerId) {
-                    item.name = this.editLedgerName
-                }
-            })
-        }, 500)
-    }
-
-    ledgersLoading = false
-
-    created() {
-        this.ledgersLoading = true
-        Client.getLedgerList().then((resp) => {
-            this.ledgersLoading = false
-            this.ledgers = resp.data
-            eventBus.$emit('ledges-changes', this.ledgers)
-        }).catch(() => {
-            this.ledgersLoading = false
-        })
-    }
+  created() {
+    this.ledgersLoading = true
+    Client.getLedgerList().then((resp) => {
+      this.ledgersLoading = false
+      // this.ledgers = resp.data
+      this.ledgers = [
+        {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },    {
+          name: 'tt'
+        },
+      ]
+      eventBus.$emit('ledges-changes', this.ledgers)
+    }).catch(() => {
+      this.ledgersLoading = false
+    })
+  }
 
 
-    pageRatio = 95
+  pageRatio = 95
 
-    mounted() {
-        this.adjustPageHeight()
-    }
+  mounted() {
+    // this.adjustPageHeight()
+  }
 
-    adjustPageHeight() {
-        let mainframe = getHtmlElem(this, "page-main-frame")
+  adjustPageHeight() {
+    // let mainframe = getHtmlElem(this, "page-main-frame")
+    //
+    // let pageMainArea = getHtmlElem(this, "page-main-area")
+    // let bottomToolBar = getHtmlElem(this, "bottom-tool-bar")
+    //
+    // let mainframeHeight = mainframe.clientHeight;
+    // let pageMainAreaHeight = mainframeHeight * this.pageRatio / 100
+    // let bottomToolBarHeight = mainframeHeight - pageMainAreaHeight
+    //
+    // pageMainArea.style.height = pageMainAreaHeight + "px"
+    // bottomToolBar.style.height = bottomToolBarHeight + "px"
+    //
+    // let e = getRef(this, "add-ledger-btn")
+    //
+    // e.style.height = "auto"
+    // e.style.padding = `${5}px`
+    // e.style.borderRadius = `${8}px`
+    // e.style.position = "relative"
+    //
+    // console.log(e.offsetHeight)
+    //
+    // // center the e to the  bottomToolBar
+    // let bottomToolBarHeight2 = bottomToolBar.offsetHeight
+    // let eHeight = e.offsetHeight
+    // let eTop = (bottomToolBarHeight2 - eHeight) / 2
+    //
+    // e.style.top = `${eTop}px`
+    //
+    // bottomToolBar.style.paddingLeft = `${8}px`
 
-        let pageMainArea = getHtmlElem(this, "page-main-area")
-        let bottomToolBar = getHtmlElem(this, "bottom-tool-bar")
+    // e.onclick = () => {
+    //     this.addLedgerName = ""
+    //     this.addLedgerLoading = false;
+    //     this.addLedgerShow = true
+    // }
+  }
 
-        let mainframeHeight = mainframe.clientHeight;
-        let pageMainAreaHeight = mainframeHeight * this.pageRatio / 100
-        let bottomToolBarHeight = mainframeHeight - pageMainAreaHeight
+  ff() {
+    this.addLedgerName = ""
+    this.addLedgerLoading = false;
+    this.addLedgerShow = true
+  }
 
-        pageMainArea.style.height = pageMainAreaHeight + "px"
-        bottomToolBar.style.height = bottomToolBarHeight + "px"
+  closed() {
 
-        let e = getRef(this, "add-ledger-btn")
-
-        e.style.height = "auto"
-        e.style.padding = `${5}px`
-        e.style.borderRadius = `${8}px`
-        e.style.position = "relative"
-
-        console.log(e.offsetHeight)
-
-        // center the e to the  bottomToolBar
-        let bottomToolBarHeight2 = bottomToolBar.offsetHeight
-        let eHeight = e.offsetHeight
-        let eTop = (bottomToolBarHeight2 - eHeight) / 2
-
-        e.style.top = `${eTop}px`
-
-        bottomToolBar.style.paddingLeft = `${8}px`
-
-        e.onclick = () => {
-            this.addLedgerName = ""
-            this.addLedgerLoading = false;
-            this.addLedgerShow = true
-        }
-    }
-
-
-    closed() {
-
-    }
+  }
 
 }
 </script>
 <style lang='scss' scoped>
 @import "~@/style/common-style.scss";
 @import "~@/style/style-specification";
-.page {
-    padding: 0 8px 0 8px;
-    background-color: #f7f8fa;
 
-}
+//.page {
+//  padding: 0 8px 0 8px;
+//  background-color: #f7f8fa;
+//}
 
 .record-header {
-    padding: 16px 16px 8px;
-    color: #969799;
-    font-size: 14px;
-    line-height: 16px;
+  padding: 16px 16px 8px;
+  color: #969799;
+  font-size: 14px;
+  line-height: 16px;
+}
+
+.bottom-bar{
+
+
+  display: flex;
+  align-items: center;
+
+
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+  border-top: 1px solid #EBECF0;
+
+  background-color: white;
+
+  display: flex;
+
+
 }
 </style>
