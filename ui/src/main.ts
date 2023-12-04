dfc78e8a58cfd937af690deb111b8d6aad837d17
wenchao <aronstrider@gmail.com>
2023-12-05 01:03:34 +0800

    [wc/clean code] clean code

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,8 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './permission'; // permission control
-import 'vant/lib/index.css';
 
+import "@/assets/icon-fonts/iconfont.css"
+
+import i18n from "@/ts/lang";
+
+Vue.config.productionTip = false;
+
+import 'vant/lib/index.css';
 import {
     ActionSheet,
     Button,
@@ -25,45 +31,33 @@ import {
     TabbarItem,
     Tag
 } from 'vant';
+{
+    Vue.use(DatetimePicker);
+    Vue.use(Loading);
+    Vue.use(Tabbar);
+    Vue.use(TabbarItem);
+    Vue.use(Notify);
+    Vue.use(Button);
+    Vue.use(Popup);
+    Vue.use(Cell);
+    Vue.use(Field);
+    Vue.use(CellGroup);
+    Vue.use(NavBar);
+    Vue.use(Icon);
+    Vue.use(List);
+    Vue.use(Tag);
+    Vue.use(DropdownMenu);
+    Vue.use(DropdownItem);
+    Vue.use(ActionSheet);
+    Vue.use(SwipeCell);
+}
+
 import PageEventbusRegistrationMixin from "@/page-eventbus-registration-mixin";
-import "@/assets/icon-fonts/iconfont.css"
-
-import i18n from "@/ts/lang";
-
-Vue.config.productionTip = false;
-import { NumberKeyboard } from 'vant';
-Vue.use(NumberKeyboard);
-
-Vue.use(DatetimePicker);
-Vue.use(Loading);
-Vue.use(Tabbar);
-Vue.use(TabbarItem);
-Vue.use(Notify);
-Vue.use(Button);
-Vue.use(Popup);
-Vue.use(Cell);
-Vue.use(Field);
-Vue.use(CellGroup);
-Vue.use(NavBar);
-Vue.use(Icon);
-Vue.use(List);
-Vue.use(Tag);
-Vue.use(DropdownMenu);
-Vue.use(DropdownItem);
-Vue.use(ActionSheet);
-
-Vue.use(SwipeCell);
-
 Vue.mixin(PageEventbusRegistrationMixin)
 
 import FastClick from 'fastclick'
 // @ts-ignore
 FastClick.attach(document.body)
-
-// Vue.prototype.$customFuncs = {
-//     getRef,
-//     getHtmlElem
-// }
 
 new Vue({
     router,
