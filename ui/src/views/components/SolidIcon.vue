f94bc523a289afc5c00f0286eb7432ab0149070e
wenchao <aronstrider@gmail.com>
2023-12-10 19:00:27 +0800

    [wc/ui] adjust icon style

--- ui/src/views/components/SolidIcon.vue
+++ ui/src/views/components/SolidIcon.vue
@@ -6,7 +6,7 @@
                     @click.native="$emit('click', $event)"
                     class="flex center"
                     style="
-                        padding: 6px 7px;
+                        padding: 6px 6px;
                         color: #ffffff;
                         background-color: #588cf3;
                         border-radius: 10px;
@@ -36,7 +36,7 @@
                     @click.native="$emit('click', $event)"
                     class="flex center"
                     style="
-                        padding: 6px 7px;
+                        padding: 6px 6px;
                         color: #ffffff;
                         background-color: #588cf3;
                         border-radius: 10px;
