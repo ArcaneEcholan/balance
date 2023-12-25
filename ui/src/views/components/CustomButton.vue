70daa0fd11532da8a3a84dcf44db3b9fb5ae8c33
wenchao <wenchao@ndsec.work>
2023-12-25 15:57:46 +0800

    [wc/feat] custom button supports disable state

--- ui/src/views/components/CustomButton.vue
+++ ui/src/views/components/CustomButton.vue
@@ -1,5 +1,9 @@
 <template>
-    <button :class="`confirm-btn ${type}`" @click="onclick">
+    <button
+        :disabled="disabled"
+        :class="`confirm-btn ${type} ${disabled ? 'disabled' : ''}`"
+        @click="onclick"
+    >
         <slot></slot>
     </button>
 </template>
@@ -11,6 +15,9 @@ export default class CommonButtonComponent extends Vue {
     // support: inline, the default style sets width to 100%;
     @Prop({ default: '' })
     type!: string;
+
+    @Prop({ default: false })
+    disabled!: boolean;
 
     onclick($event: any) {
         this.$emit('click', $event);
@@ -39,6 +46,10 @@ export default class CommonButtonComponent extends Vue {
         padding: 4px 8px;
         font-size: 14px;
         border-radius: 20px;
+    }
+
+    &.disabled {
+        opacity: 0.5;
     }
 }
 </style>
